344c425ba35516214a9eec672ceb575a2e4fc532
shreeyuuummm <shreemhyd24@gmail.com>
2025-03-29 09:45:14 +0530

    Corrected Script

---
+++ script.py
@@ -0,0 +1,14 @@
+def find_cube_pairs(target) :  #a colon is required to define the function
+    solutions = []   ##no semicolon required
+    max_num = round(target * (1/3))   #one asterisk required for multiplication and not 3,parameter is called target and not targ
+
+    for a in range(1, max_num + 1):   #it should be range and not ranges,also colon expected after for statement
+        for b in range(a, max_num + 1):      #colon expected after for statement,it should be range and not ranges
+            if a*3 + b*3 == target:         #colon expected after if statement,only one * required for multiplication and not three,parameter is called target and not targ
+                solutions.append((a, b))       #no semi-colon required,string name is solutions and not sol
+    return solutions
+
+pairs = find_cube_pairs(1729),
+print("Valid cube pairs for 1728:"),       #python uses print and not printf
+for a, b in pairs:                        #colon needed for the for statement,the list name is pairs and not pair
+    print(f" → {a}³ + {b}³ = {a**2} + {b**2} = 1728")     #python uses print and not printf
