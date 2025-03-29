a876bf58f58dc1970e406ac5441d0bd023232f49
shreeyuuummm <shreemhyd24@gmail.com>
2025-03-29 09:59:38 +0530

    Corrected script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def find_cube_pairs(target) :  #a colon is required to define the function
         for b in range(a, max_num + 1):      #colon expected after for statement,it should be range and not ranges
             if a**3 + b**3 == target:         #colon expected after if statement,only two * required for exponentiation and not three,parameter is called target and not targ
                 solutions.append((a, b))       #no semi-colon required,string name is solutions and not sol
-    return solutions
+    return solutions   #changed sol to soltuions
 
 pairs = find_cube_pairs(1729),
 print("Valid cube pairs for 1728:"),       #python uses print and not printf
