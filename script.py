e053f1b5a091bcbe412fb70c9db9e21f8dc4deda
shreeyuuummm <shreemhyd24@gmail.com>
2025-03-29 09:57:37 +0530

    Corrected script.py

--- script.py
+++ script.py
@@ -11,4 +11,4 @@ def find_cube_pairs(target) :  #a colon is required to define the function
 pairs = find_cube_pairs(1729),
 print("Valid cube pairs for 1728:"),       #python uses print and not printf
 for a, b in pairs:                        #colon needed for the for statement,the list name is pairs and not pair
-    print(f" → {a}³ + {b}³ = {a**2} + {b**2} = 1728")     #python uses print and not printf
+    print(f" → {a}³ + {b}³ = {a**3} + {b**3} = 1728")     #python uses print and not printf,we are calculating cubes and not squares so changed 2 to 3
